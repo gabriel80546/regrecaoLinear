a75c2189bd50b9b14a1edf0439b87d0d6a19df9b
Gabriel <gabriel80546@gmail.com>
2023-05-22 11:42:43 -0300

    ./push.sh

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int numeros[30], i;
+    for(i = 0; i < 30; i++) {
+        numeros[i] = 0;
+    }
+    for(i = 0; i < 30; i++) {
+        printf("diga um número: ");
+        scanf("%i", &numeros[i]);
+        if(&numeros[i] <= 0) {
+            break;
+        }
+    }
+
+    for(i = 0; i < 30; i++) {
+        printf("numeros[%i] = %i\n", i, numeros[i]);
+    }
+    printf("Olá Mundo!\n");
+    return 0;
+}
